c9eedac84897a63618fb61654d7b7e77917d94d4
M4d Sc13nT15t & M4d Ch3m15t <31043597+M3nT4LH05p1T4LC0d3r5@users.noreply.github.com>
2018-01-28 13:02:00 -0500

    Security Scans Using Web Browser
    Ruby Scripts Designed to Run Non-Intrusive Security Tests

---
+++ WpUporDown.rb
@@ -0,0 +1,58 @@
+require 'socket'
+require 'tor_requests'
+require 'dnsruby'
+require 'geocoder' 
+require 'resolv'
+require 'net/http'
+require 'net/https'
+require 'celluloid'
+require 'timeout'
+require 'colorize'
+require 'open-uri'
+require 'net/ping'
+require 'rubygems'
+require 'mechanize'
+require 'resolv'
+require 'spidr'
+require 'nokogiri'
+
+puts """
+
+                            ,ooo888888888888888oooo,
+                          o8888YYYYYY77iiiiooo8888888o
+                         8888YYYY77iiYY8888888888888888
+                        [88YYY77iiY88888888888888888888]
+                        88YY7iYY888888888888888888888888
+                       [88YYi 88888888888888888888888888]
+                       i88Yo8888888888888888888888888888i
+                       i]        ^^^88888888^^^     o  [i
+                      oi8  i           o8o          i  8io
+                    ,77788o ^^  ,oooo8888888ooo,   ^ o88777,
+                    7777788888888888888888888888888888877777
+                     77777888888888888888888888888888877777
+                      77777788888888^7777777^8888888777777
+       ,oooo888 ooo   88888778888^7777ooooo7777^8887788888        ,o88^^^^888oo
+    o8888777788[];78 88888888888888888888888888888888888887 7;8^ 888888888oo^88
+   o888888iii788 ]; o 78888887788788888^;;^888878877888887 o7;[]88888888888888o
+   88888877 ii78[]8;7o 7888878^ ^8788^;;;;;;^878^ ^878877 o7;8 ]878888888888888
+  [88888888887888 87;7oo 777888o8888^;ii;;ii;^888o87777 oo7;7[]8778888888888888
+  88888888888888[]87;777oooooooooooooo888888oooooooooooo77;78]88877i78888888888
+ o88888888888888 877;7877788777iiiiiii;;;;;iiiiiiiii77877i;78] 88877i;788888888
+ 88^;iiii^88888 o87;78888888888888888888888888888888888887;778] 88877ii;7788888
+;;;iiiii7iiii^  87;;888888888888888888888888888888888888887;778] 888777ii;78888
+;iiiii7iiiii7iiii77;i88888888888888888888i7888888888888888877;77i 888877777ii78
+iiiiiiiiiii7iiii7iii;;;i7778888888888888ii7788888888888777i;;;;iiii 88888888888
+
+"""
+
+
+begin
+a = Mechanize.new
+print " Who's Network To Scan? --> "
+phoney = gets.chomp
+
+a = Mechanize.new
+
+
+system("firefox https://www.google.ca/search?q=site:#{phoney}+inurl:wp-+OR+inurl:plugin+OR+inurl:upload+OR+inurl:download")
+end
